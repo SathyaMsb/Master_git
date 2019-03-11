19939a785c69232ed15d3597f4f9bd9d3210ddd0
Sathya <sathyamsb91@gmail.com>
2019-03-11 19:18:24 +0530

    Java

--- Hello.java
+++ Hello.java
@@ -1,17 +1,7 @@
-public class Fibonacci {
+public class Hello {
 
     public static void main(String[] args) {
 
-        int n = 10, t1 = 0, t2 = 1;
-        System.out.print("First " + n + " terms: ");
-
-        for (int i = 1; i <= n; ++i)
-        {
-            System.out.print(t1 + " + ");
-
-            int sum = t1 + t2;
-            t1 = t2;
-            t2 = sum;
-        }
-    }
+        System.out.println( " Hello, World ");
+}
 }
