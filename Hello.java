d39e5cc60c0e430e5dd8b18fd082f2128ea16902
Sathya <sathyamsb91@gmail.com>
2019-03-11 19:03:56 +0530

    Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,17 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
